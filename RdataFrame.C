b4d5074872415ad6e07d0960afd1ad45000f67ac
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-01-21 20:38:47 +0100

    temp

--- RdataFrame.C
+++ RdataFrame.C
@@ -16,8 +16,35 @@
  * =====================================================================================
  */
 
+// ROOT::EnableImplicitMT(); // uncomment to run in parallel mode
+// using TLorentzVectors = vector<TLorentzVector>;
+// using doubles = vector<double>;
+// using crvecRef = const ROOT::VecOps::RVec<double>&;
+
+// const TLorentzVector CalcInvMass(double pt, double eta, double phi, double mass) {
+//         TLorentzVector lv;
+//         lv.SetPtEtaPhiM(pt, eta, phi, mass);
+//         return lv;
+// }
+
+// doubles CalcInvMasses(int nParts, crvecRef pts, crvecRef etas, crvecRef phis, crvecRef masses)
+// {
+//    doubles masses(nParts);
+//    for (auto i : ROOT::TSeqU(nParts)) {
+//       masses[i] = CalcInvMass(pts[i], etas[i], phis[i], masses[i]);
+//    }
+//    return masses;
+// }
+
+double testFunc(float test) {
+    return test;
+}
+
 void RdataFrame() {
-    ROOT::RDataFrame df("tagsDumper/trees/Data_13TeV_HHWWggTag_1", "/eos/user/a/atishelm/ntuples/HHWWgg_flashgg/January_2021_Production/2017/Data_Trees/Data_2017.root");
-    // df.Filter("") // some cut expression -- you can also pass a C++ function
-    df.Snapshot("Data_13TeV_HHWWggTag_1", "RESULT.root"); // write to file all events that pass the filter
+    // ROOT::RDataFrame rdf("tagsDumper/trees/Data_13TeV_HHWWggTag_1", "/eos/user/a/atishelm/ntuples/HHWWgg_flashgg/January_2021_Production/2017/Data_Trees/Data_2017.root");
+    ROOT::RDataFrame rdf("tagsDumper/trees/Data_13TeV_HHWWggTag_1", "test.root");
+    // auto CMS_hgg_mass = rdf.Histo1D("CMS_hgg_mass");
+    auto rdf1 = rdf.Define("testFunc", testFunc, {"CMS_hgg_mass"});
+    auto newDf = rdf.Snapshot("Data_13TeV_HHWWggTag_1", "outNameFile.root");
+    auto newHist = rdf1.Histo1D("testFunc");
 }
