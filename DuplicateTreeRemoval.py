b34721ae6a52bf9bbb016ab6bb2ccc827fd0d856
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-02-03 21:31:59 +0100

    Updated DuplicateTreeRemoval.py script

--- DuplicateTreeRemoval.py
+++ DuplicateTreeRemoval.py
@@ -2,48 +2,65 @@
 # @Author: Ram Krishna Sharma
 # @Date:   2021-02-03 12:49:29
 # @Last Modified by:   ramkrishna
-# @Last Modified time: 2021-02-03 14:14:32
+# @Last Modified time: 2021-02-03 22:00:17
 
-# -*- coding: utf-8 -*-
-# @Author: Ram Krishna Sharma
-# @Date:   2021-01-29 01:24:33
-# @Last Modified by:   ramkrishna
-# @Last Modified time: 2021-01-29 02:56:28
-
-from ROOT import TFile
 import os
+import sys
 import argparse
+from os import listdir
+from os.path import isfile, join
 
 parser = argparse.ArgumentParser(description="""
-    This script is going check root file and remove the duplicate trees.
-    """)
-parser.add_argument("--InFile", default="", help="Name of Employee")
-parser.add_argument("--InPath", default="", help="Job Title of Employee")
-parser.add_argument("--OutPath", default="", help="Address of Employee")
-
+                    This script is going check root file and remove the duplicate trees.
+                    """)
+parser.add_argument("--InFile",
+                    default="",
+                    help="Name of input root file",
+                    required=False)
+parser.add_argument("--InPath",
+                    default="",
+                    required=True,
+                    help="Path where input files are present")
+parser.add_argument("--OutPath",
+                    default="",
+                    help="""
+                        Path where output file will be stored.
+                        In case no output path specified it creates a directory
+                        named "hadd" in in the input file path and sends hadd-ed
+                        file there.
+                        """)
+parser.add_argument("--IsMany",
+                    default=False,
+                    help="Need to hadd only one file or all files in the input directory?")
 args = parser.parse_args()
 
-if args.InFile == "":
-    InputRootFile = "GluGluToHHTo2G2ZTo2G4Q_node_cHHH1_2016.root"
-else:
-    InputRootFile = args.InFile
+"""
+[ISSUE] If we import ROOT before argparse then ROOT loads its own help message instead of the
+python message that  we would like to print. So, always load ROOT after the argparse.
+"""
+from ROOT import TFile
 
-if args.InPath == "":
-    InputRootFilePath = "/eos/user/r/rasharma/post_doc_ihep/double-higgs/ntuples/ReRunFHJetSelection_January_2021_Production/AllBVetoJets/BTagFix_2016"
-else:
-    InputRootFilePath = args.InPath
-
+InputRootFile = args.InFile
+if InputRootFile == "":
+    args.IsMany = True
+InputRootFilePath = args.InPath
 if args.OutPath == "":
     # OutputRootFilePath = "/tmp/rasharma"
-    OutputRootFilePath = InputRootFilePath+os.sep+"hadd"
+    OutputRootFilePath = (InputRootFilePath+os.sep+"hadd").replace("//","/")
     os.system("mkdir "+OutputRootFilePath)
 else:
     OutputRootFilePath = args.OutPath
 
 IfDryRun = False
 
-print "Input Root file: ",InputRootFilePath,InputRootFile
+print "Input Path: ",InputRootFilePath
+print "Input Root file: ",InputRootFile
 print "OutputRootFilePath: ",OutputRootFilePath
+
+if args.IsMany:
+    onlyfiles = [f for f in listdir(InputRootFilePath) if isfile(join(InputRootFilePath, f))]
+else:
+    onlyfiles = [InputRootFile]
 
 
 def getallTrees(d, basepath="/", verbose=False):
@@ -56,30 +73,36 @@ def getallTrees(d, basepath="/", verbose=False):
         else:
             yield basepath+kname, d.Get(kname)
 
-contains_duplicates = True
 
-InputFileWithPath = InputRootFilePath+os.sep+InputRootFile
+print "onlyfiles:"
+print onlyfiles
+for count, InputRootFile in enumerate(onlyfiles):
+    print "\n\n"
+    contains_duplicates = True
+    InputFileWithPath = InputRootFilePath+os.sep+InputRootFile
+    print "File (",str(count)+"/"+str(len(onlyfiles)),"): ",InputFileWithPath
+    while contains_duplicates:
+        file = TFile.Open(InputFileWithPath)
+        rootobjects_raw =  list(getallTrees(file))
 
-while contains_duplicates:
-    file = TFile.Open(InputFileWithPath)
-    rootobjects_raw =  list(getallTrees(file))
+        OnlyTreesNames = [item[0] for item in rootobjects_raw]
 
-    OnlyTreesNames = [item[0] for item in rootobjects_raw]
+        NoDuplicates_TreesNames = set(OnlyTreesNames)
 
-    NoDuplicates_TreesNames = set(OnlyTreesNames)
+        contains_duplicates = len(OnlyTreesNames) != len(NoDuplicates_TreesNames)
 
-    contains_duplicates = len(OnlyTreesNames) != len(NoDuplicates_TreesNames)
-
-    # print("contains_duplicates: ",contains_duplicates)
-    OutputRootFileWithPath = ""
-    if contains_duplicates:
-        print("Found duplicate trees. So, running the hadd command to remove duplicate trees.")
-        OutputRootFileWithPath = "/tmp/rasharma"+os.sep+InputRootFile
-        hadd_command = "hadd -f "+OutputRootFileWithPath+" "+InputFileWithPath
-        print("===> "+hadd_command)
-        if not IfDryRun: os.system(hadd_command)
-    else:
-        print("No duplicate trees found...")
-        contains_duplicates = False
-        os.system("mv "+InputFileWithPath+ " " + OutputRootFilePath+os.sep)
-    InputFileWithPath = OutputRootFileWithPath
+        # print("contains_duplicates: ",contains_duplicates)
+        OutputRootFileWithPath = ""
+        if contains_duplicates:
+            print("Found duplicate trees. So, running the hadd command to remove duplicate trees.")
+            OutputRootFileWithPath = "/tmp/rasharma"+os.sep+InputRootFile
+            hadd_command = "hadd -f "+OutputRootFileWithPath+" "+InputFileWithPath
+            print("===> "+hadd_command)
+            if not IfDryRun: os.system(hadd_command)
+        else:
+            print("No duplicate trees found...")
+            contains_duplicates = False
+            move_command = "mv "+InputFileWithPath+ " " + OutputRootFilePath+os.sep
+            print "Running command:\n\t",move_command
+            os.system(move_command)
+        InputFileWithPath = OutputRootFileWithPath
