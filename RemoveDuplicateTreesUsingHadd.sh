f6fac16ee7358529d2c9ab5b80522107b4e319ea
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-04-20 09:18:16 +0200

    Updated script RemoveDuplicateTreesUsingHadd.sh

--- RemoveDuplicateTreesUsingHadd.sh
+++ RemoveDuplicateTreesUsingHadd.sh
@@ -2,25 +2,38 @@
 # @Date:   2021-04-12 17:40:46
 # @Last Modified by:   Ram Krishna Sharma
 # @Last Modified time: 2021-04-19
-path1="/eos/user/r/rasharma/post_doc_ihep/double-higgs/ntuples/January_2021_Production/DNN_MoreVar_v3_WithSyst_2017/Signal/*cHHH1_2017.root"
+#path1="/eos/user/r/rasharma/post_doc_ihep/double-higgs/ntuples/January_2021_Production/DNN_MoreVar_v3_WithSyst_2017/Signal/GluGluToHHTo2G4Q_node_cHHH1_2017.root"
+# path1="/eos/user/r/rasharma/post_doc_ihep/double-higgs/ntuples/January_2021_Production/DNN_MoreVar_v3_WithSyst_2017/Signal/*cHHH1_2017.root"
 # path1="/eos/user/r/rasharma/post_doc_ihep/double-higgs/ntuples/January_2021_Production/DNN_MoreVar_v3_WithSyst_2017/Backgrounds/*.root"
 # path1="/eos/user/r/rasharma/post_doc_ihep/double-higgs/ntuples/January_2021_Production/DNN_MoreVar_v3_WithSyst_2017/Data/*.root"
 
+path1="/eos/user/l/lipe/ntuple/DNN_sample/FlashggNtuples_WithMoreVars/2016/*cHHH1_2016.root"
+
 doHadd=${1}
 DirName="tagsDumper/trees"
-DirName=""
+# DirName=""
 
 if [[ doHadd -eq 1 ]]; then
     #statements
     for filename in ${path1}
     do
+        echo "==============================================================="
+        echo ""
+        date
+        echo ""
+        echo "==============================================================="
         NewFileName="${filename%.root}"
         NewFileName="${NewFileName}_hadd.root"
+        echo "Before hadd keep original file to /tmp/rasharma"
+        echo "In case something happens we can grab them from /tmp/rasharma"
+        cp $filename /tmp/rasharma/
         echo "hadd $NewFileName $filename"
         hadd $NewFileName $filename
         echo "---"
         echo "mv $NewFileName $filename"
         mv $NewFileName $filename
+        echo "==========="
+        date
         echo "==========="
     done
 fi
